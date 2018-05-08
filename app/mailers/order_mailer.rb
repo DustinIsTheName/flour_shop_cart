5e84a63ff7b7395e5e7c48d52c55dd232e84bdf5
Dustin Milam <darklordsushi@yahoo.com>
2018-05-08 12:10:04 -0400

    added cc of info@flourshop.com

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -2,7 +2,7 @@ class OrderMailer < ApplicationMailer
 
   def order_accepted(order)
   	@order = order
-    mail(to: order.email, subject: "ORDER #{@order.number}")
+    mail(to: order.email, cc: "info@flourshop.com", subject: "ORDER #{@order.number}")
   end
 
 end
