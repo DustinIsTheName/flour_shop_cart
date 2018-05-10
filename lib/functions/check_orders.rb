285a4e5e921f535e4032b7de58bc7244e407e20d
Dustin Milam <darklordsushi@yahoo.com>
2018-05-10 15:04:27 -0400

    fulfill rake task now goes through all pages of orders

--- lib/functions/check_orders.rb
+++ lib/functions/check_orders.rb
@@ -1,32 +1,36 @@
 class CheckOrders
 
 	def self.fulfill
-		orders = ShopifyAPI::Order.all
-		date_now = DateTime.now.strftime('%Y/%m/%d')
+		pages = (ShopifyAPI::Order.count/250.0).ceil
 
-		for order in orders
-			if order.tags.split(', ').include? 'fulfilled'
-				puts Colorize.cyan(date_now)
-				puts Colorize.magenta(order.note_attributes&.select{|a| a.name == 'Pickup-Date'}&.first&.value)
+		for page in 1..pages
+			orders = ShopifyAPI::Order.find(:all, params: {limit: 250, page: page})
+			date_now = DateTime.now.strftime('%Y/%m/%d')
 
-				if order.note_attributes&.select{|a| a.name == 'Pickup-Date'}&.first&.value == date_now
-					puts order.name
-					order.tags = order.tags.remove_tag 'fulfilled'
-					
-					f = ShopifyAPI::Fulfillment.new
-					f.prefix_options[:order_id] = order.id
-					f.notify_customer = true
+			for order in orders
+				if order.tags.split(', ').include? 'fulfilled'
+					puts Colorize.cyan(date_now)
+					puts Colorize.magenta(order.note_attributes&.select{|a| a.name == 'Pickup-Date'}&.first&.value)
 
-					if f.save
-						puts Colorize.green('saved Fulfillment')
-					else
-						puts Colorize.red('error saving Fulfillment')
-					end
+					if order.note_attributes&.select{|a| a.name == 'Pickup-Date'}&.first&.value == date_now
+						puts order.name
+						order.tags = order.tags.remove_tag 'fulfilled'
+						
+						f = ShopifyAPI::Fulfillment.new
+						f.prefix_options[:order_id] = order.id
+						f.notify_customer = true
 
-					if order.save
-						puts Colorize.green('saved order')
-					else
-						puts Colorize.red('error saving order')
+						if f.save
+							puts Colorize.green('saved Fulfillment')
+						else
+							puts Colorize.red('error saving Fulfillment')
+						end
+
+						if order.save
+							puts Colorize.green('saved order')
+						else
+							puts Colorize.red('error saving order')
+						end
 					end
 				end
 			end
